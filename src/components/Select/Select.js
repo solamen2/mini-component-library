2e0cbb645d6927e12f1bc3545d056da809fd8b12
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-01-09 16:50:29 -0800

    Implement Select (mostly, and very hackily :-/ )

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ label, value, onChange, children}) => {
   const displayedValue = getDisplayedValue(value, children);
 
+  const canvasContext = document.createElement('canvas').getContext('2d');
+  const textWidth = Math.ceil(canvasContext.measureText(displayedValue).width);
+
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Fragment>
+      <Wrapper value={value} onChange={onChange}>
+        {children}
+      </Wrapper>
+      <Chevron style={{ "padding": "0px 0px 0px " + ((textWidth * 1.4) + 24) + "px" }}>
+        <Icon id="chevron-down" size="24"/>
+      </Chevron>
+    </Fragment>
   );
 };
+
+const Chevron = styled.div`
+  margin: -30px 0px 0px 0px;
+  color: ${COLORS.gray700};
+`;
+
+const Wrapper = styled.select`
+  appearance:none;
+  border-radius: 8px;
+  padding: 10px 14px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:hover + ${Chevron} {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
